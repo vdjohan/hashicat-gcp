3ada77614c6000ac278c8d8a9c8ff76fea47e420
Vera Williams <vera.williams@nokia.com>
2021-01-15 20:46:08 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -5,10 +5,10 @@ module "network" {
   network_name = "gaurav-network"
   project_id = var.project
 
-    subnets = 
-    {
-        subnet_name   = "gaurav-subnet"
-        subnet_ip     = "10.100.10.0/24"
-        subnet_region = var.region
-    }
+  subnets = 
+ {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+ }
 }
