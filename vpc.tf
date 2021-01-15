b554bfc22cab151c4ed94fe1a5c19a49c256ebc8
Vera Williams <vera.williams@nokia.com>
2021-01-15 20:41:01 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/VERA-training/network/google"
+  version = "3.0.0"
+  # insert required variables here
+  network_name = "gaurav-network"
+  project_id = var.project
+}
+subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
